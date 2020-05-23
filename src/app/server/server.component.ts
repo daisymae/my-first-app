21b0079f8bde73f7b9bb04debc6eef3e6156a346
daisymae <daisymae.orcadiafarm@gmail.com>
2020-05-23 14:23:09 -0500

    Databinding
    String interpolation

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,9 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server', // unique name
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
 })
 export class ServerComponent {
+  // alternate syntax: name: type = value; i.e. serverId: number = 10;
+  serverId = 10;
+  serverStatus = 'offline';
 
-
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Server } from 'http';
 
 @Component({
   selector: 'app-servers', // element style selector - most common use
   // selector: '[app-servers]', // [] makes it an attribute selector
   // selector: '.app-servers', // '.' makes it a class selector
-  template: ` <app-server></app-server>
+  template: `
+    <app-server></app-server>
     <app-server></app-server>`, // './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
